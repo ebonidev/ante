9acd7a7e883c8254ec771420d9514bdeacbf88d2
Eboni Evans <eboni.evans@gmail.com>
2022-10-04 13:12:58 -0500

    removed mongoose from server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
 const session = require('express-session');
